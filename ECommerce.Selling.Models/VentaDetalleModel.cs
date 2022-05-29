75edef9e4fbc4669393bdb91186f556b9c4d0d50
99100043 <raedmiranda>
2022-05-29 12:55:00 -0500

    Agregar archivos de proyecto.

---
+++ ECommerce.Selling.Models/VentaDetalleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Selling.Models
+{
+    public class VentaDetalleModel
+    {
+        public string IdVenta { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
---
+++ ECommerce.Selling.Models/VentaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ECommerce.Selling.Models
+{
+    public class VentaModel
+    {
+        public string Id { get; set; }
+        public int IdUsuario { get; set; }
+        public decimal precioTotal { get; set; }
+        public string medioDePago { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
